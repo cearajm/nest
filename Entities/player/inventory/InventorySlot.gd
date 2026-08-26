a5fa4c57cf1e3c092db163661d55f4eb5382f5d2
cearajm <cearamahinay@gmail.com>
2026-08-26 04:43:58 -0600

    feat: item slot handling

--- Entities/player/inventory/InventorySlot.gd
+++ Entities/player/inventory/InventorySlot.gd
@@ -1,6 +1,6 @@
 extends Node
 class_name InventorySlot
-
+# manages the item and its attributes in a slot
 
 @onready var icon: TextureRect = get_node("Icon")
 @onready var quantity_text: Label = get_node("QuantityText")
@@ -14,13 +14,39 @@ func _ready() -> void:
 	
 
 func set_item(new_item: Item):
-	pass
-	
-func add_item():
-	pass
-	
-func remove_item():
-	pass
-	
+	# assign item to an item variable, and qty == 1 by default
+	item = new_item
+	quantity = 1
+	if item == null:
+		icon.visible = true  # empty the slot
+	else:
+		icon.visible = true  # show item image
+		icon.texture = item.icon
+	update_quantity_text()
+
+
 func update_quantity_text():
-	pass
+	if quantity <= 1:
+		quantity_text.text = ""
+	else:
+		quantity_text.text = str(quantity)
+
+
+func add_item():
+	quantity += 1
+	update_quantity_text()
+
+func remove_item():
+	quantity -= 1
+	update_quantity_text()
+	if quantity == 0:
+		set_item(null)
+
+
+func _on_pressed() -> void:
+	if item == null:
+		return
+		
+	var remove_after_use = item._on_use(inventory.get_parent())
+	if remove_after_use:
+		remove_item()
